5d13c4b601bbfb1faedcda10e92a56fe64c47235
jamestheengineer <jamestheengineer@gmail.com>
2025-01-19 10:45:06 -0500

    playing around

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -2,20 +2,11 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-    std.log.info("Zig version: {}", .{ builtin.zig_version });
-
-    try bw.flush(); // don't forget to flush!
+    const msg = "hello this is dog";
+    var it = std.mem.tokenize(u8, msg, " ");
+    while (it.next()) |item| {
+        std.debug.print("{s}\n", .{item});
+    }
 }
 
 test "simple test" {
@@ -23,4 +14,9 @@ test "simple test" {
     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "integer overflow at compile time" {
+    const x: u8 = 255;
+    x += 1;
 }
