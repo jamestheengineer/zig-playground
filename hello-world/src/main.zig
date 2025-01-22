6874bc602a873f5972a23d71833f4cd8fbb1bc89
jamestheengineer <jamestheengineer@gmail.com>
2025-01-22 18:17:30 -0500

    strings and unicode

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,71 +1,19 @@
-const Header = struct {
-    magic: u32,
-    name: []const u8,
-};
-
-const print = std.debug.print;
-const std = @import("std");
-const os = std.os;
-const assert = std.debug.assert;
+const print = @import("std").debug.print;
+const mem = @import("std").mem; // will be used to compare bytes
 
 pub fn main() void {
-    printInfoAboutStruct(Header);
-
-    // integers
-    const one_plus_one: i32 = 1 + 1;
-    print("1 + 1 = {}\n", .{one_plus_one});
-
-    // floats
-    const seven_div_three: f32 = 7.0 / 3.0;
-    print("7.0 / 3.0 = {}\n", .{seven_div_three});
-
-    // boolean
-    print("{}\n{}\n{}\n", .{
-        true and false,
-        true or false,
-        !true,
-    });
-
-    // optional
-    var optional_value: ?[]const u8 = null;
-    assert(optional_value == null);
-
-    print("\noptional 1\ntype: {}\nvalue: {?s}\n", .{
-        @TypeOf(optional_value), optional_value,
-    });
-
-    optional_value = "hi";
-    assert(optional_value != null);
-
-    print("\noptional 2\ntype: {}\nvalue: {?s}\n", .{
-        @TypeOf(optional_value), optional_value,
-    });
-
-    // error union
-    var number_or_error: anyerror!i32 = error.ArgNotFound;
-
-    print("\nerror union 1\ntype: {}\nvalue: {!}\n", .{
-        @TypeOf(number_or_error),
-        number_or_error,
-    });
-
-    number_or_error = 1234;
-
-    print("\nerror union 2\ntype: {}\nvalue: {!}\n", .{
-        @TypeOf(number_or_error), number_or_error,
-    });
-}
-
-fn printInfoAboutStruct(comptime T: type) void {
-    const info = @typeInfo(T);
-    inline for (info.Struct.fields) |field| {
-        std.debug.print(
-            "{s} has a field called {s} with type {s}\n",
-            .{
-                @typeName(T),
-                field.name,
-                @typeName(field.type),
-            },
-        );
-    }
+    const bytes = "hello";
+    print("{}\n", .{@TypeOf(bytes)}); // *const [5:0]u8
+    print("{d}\n", .{bytes.len}); // 5
+    print("{c}\n", .{bytes[1]}); // 'e'
+    print("{d}\n", .{bytes[5]}); // 0
+    print("{}\n", .{'e' == '\x65'}); // true
+    print("{d}\n", .{'\u{1f4a9}'}); // 128169
+    print("{d}\n", .{'💯'}); // 128175
+    print("{u}\n", .{'⚡'});
+    print("{}\n", .{mem.eql(u8, "hello", "h\x65llo")}); // true
+    print("{}\n", .{mem.eql(u8, "💯", "\xf0\x9f\x92\xaf")}); // also true
+    const invalid_utf8 = "\xff\xfe"; // non-UTF-8 strings are possible with \xNN notation.
+    print("0x{x}\n", .{invalid_utf8[1]}); // indexing them returns individual bytes...
+    print("0x{x}\n", .{"💯"[1]}); // ...as does indexing part-way through non-ASCII characters
 }
