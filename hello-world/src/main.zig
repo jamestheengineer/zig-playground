93f07bd2d0ef45632468a597dc2c88ceecc9abd0
jamestheengineer <jamestheengineer@gmail.com>
2025-01-31 17:58:37 -0500

    slices

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,55 +1,50 @@
-const expect = @import("std").testing.expect;
-const expectEqualSlices = @import("std").testing.expectEqualSlices;
+const std = @import("std");
+const expect = std.testing.expect;
+const mem = std.mem;
+const fmt = std.fmt;
 
-test "basic slices" {
-    var array = [_]i32{ 1, 2, 3, 4 };
-    var known_at_runtime_zero: usize = 0;
-    _ = &known_at_runtime_zero;
-    const slice = array[known_at_runtime_zero..array.len];
+test "using slices for strings" {
+    // Zig has no concept of strings. String literals are const pointers
+    // to null-terminated arrays of u8, and by convention parameters
+    // that are "strings" are expected to be UTF-8 encoded slices of u8.
+    // Here we coerce *const [5:0]u8 and *const [6:0]u8 to []const u8
+    const hello: []const u8 = "hello";
+    const world: []const u8 = "世界";
 
-    // alternative initialization using result location
-    const alt_slice: []const i32 = &.{ 1, 2, 3, 4 };
+    var all_together: [100]u8 = undefined;
+    // You can use slice syntax with at least one runtime-known index on an
+    // array to convert an array into a slice.
+    var start: usize = 0;
+    _ = &start;
+    const all_together_slice = all_together[start..];
+    // String concatenation example.
+    const hello_world = try fmt.bufPrint(all_together_slice, "{s} {s}", .{ hello, world });
 
-    try expectEqualSlices(i32, slice, alt_slice);
+    // Generally, you can use UTF-8 and not worry about whether something is a
+    // string. If you don't need to deal with individual characters, no need
+    // to decode.
+    try expect(mem.eql(u8, hello_world, "hello 世界"));
+}
 
-    try expect(@TypeOf(slice) == []i32);
-    try expect(&slice[0] == &array[0]);
-    try expect(slice.len == array.len);
+test "slice pointer" {
+    var array: [10]u8 = undefined;
+    const ptr = &array;
+    try expect(@TypeOf(ptr) == *[10]u8);
 
-    // If you slice with comptime-known start and end positions, the result is
-    // a pointer to an array, rather than a slice.
-    const array_ptr = array[0..array.len];
-    try expect(@TypeOf(array_ptr) == *[array.len]i32);
+    // A pointer to an array can be sliced just like an array:
+    var start: usize = 0;
+    var end: usize = 5;
+    _ = .{ &start, &end };
+    const slice = ptr[start..end];
+    // The slice is mutable because we sliced a mutable pointer.
+    try expect(@TypeOf(slice) == []u8);
+    slice[2] = 3;
+    try expect(array[2] == 3);
 
-    // You can perform a slice-by-length by slicing twice. This allows the compiler
-    // to perform some optimisations like recognising a comptime-known length when
-    // the start position is only known at runtime.
-    var runtime_start: usize = 1;
-    _ = &runtime_start;
-    const length = 2;
-    const array_ptr_len = array[runtime_start..][0..length];
-    try expect(@TypeOf(array_ptr_len) == *[length]i32);
-
-    // Using the address-of operator on a slice gives a single-item pointer.
-    try expect(@TypeOf(&slice[0]) == *i32);
-    // Using the `ptr` field gives a many-item pointer.
-    try expect(@TypeOf(slice.ptr) == [*]i32);
-    try expect(@intFromPtr(slice.ptr) == @intFromPtr(&slice[0]));
-
-    // Slices have array bounds checking. If you try to access something out
-    // of bounds, you'll get a safety check failure:
-    // slice[10] += 1;
-
-    // Note that `slice.ptr` does not invoke safety checking, while `&slice[0]`
-    // asserts that the slice has len > 0.
-
-    // Empty slices can be created like this:
-    const empty1 = &[0]u8{};
-    // If the type is known you can use this short hand:
-    const empty2: []u8 = &.{};
-    try expect(empty1.len == 0);
-    try expect(empty2.len == 0);
-
-    // A zero-length initialization can always be used to create an empty slice, even if the slice is mutable.
-    // This is because the pointed-to data is zero bits long, so its immutability is irrelevant.
+    // Again, slicing with comptime-known indexes will produce another pointer
+    // to an array:
+    const ptr2 = slice[2..3];
+    try expect(ptr2.len == 1);
+    try expect(ptr2[0] == 3);
+    try expect(@TypeOf(ptr2) == *[1]u8);
 }
