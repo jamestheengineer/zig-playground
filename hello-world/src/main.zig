8ea4a64a338506b42e971856e2fc30e9e795d556
jamestheengineer <jamestheengineer@gmail.com>
2025-02-04 19:19:44 -0500

    zig tuples

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,14 +1,19 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
-pub fn main() void {
-    const Foo = struct {};
-    std.debug.print("variable: {s}\n", .{@typeName(Foo)});
-    std.debug.print("anonymous: {s}\n", .{@typeName(struct {})});
-    std.debug.print("function: {s}\n", .{@typeName(List(i32))});
-}
-
-fn List(comptime T: type) type {
-    return struct {
-        x: T,
-    };
+test "tuple" {
+    const values = .{
+        @as(u32, 1234),
+        @as(f64, 12.34),
+        true,
+        "hi",
+    } ++ .{false} ** 2;
+    try expect(values[0] == 1234);
+    try expect(values[4] == false);
+    inline for (values, 0..) |v, i| {
+        if (i != 2) continue;
+        try expect(v);
+    }
+    try expect(values.len == 6);
+    try expect(values.@"3"[0] == 'h');
 }
