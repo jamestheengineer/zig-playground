ae01d8f87cc823b329f1559df26a7c486c80440c
jamestheengineer <jamestheengineer@gmail.com>
2025-02-11 17:30:10 -0500

    switch statements in zig

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,64 +1,36 @@
-const std = @import("std");
-const builtin = @import("builtin");
-const expect = std.testing.expect;
+const expect = @import("std").testing.expect;
 
-test "switch simple" {
-    const a: u64 = 10;
-    const zz: u64 = 103;
-
-    // All branches of a switch expression must be able to be coerced to a
-    // common type.
-    //
-    // Branches cannot fallthrough. If fallthrough behavior is desired, combine
-    // the cases and use an if.
-    const b = switch (a) {
-        // Multiple cases can be combined via a ','
-        1, 2, 3 => 0,
-
-        // Ranges can be specified using the ... syntax. These are inclusive
-        // of both ends.
-        5...100 => 1,
-
-        // Branches can be arbitrarily complex.
-        101 => blk: {
-            const c: u64 = 5;
-            break :blk c * 2 + 1;
-        },
-
-        // Switching on arbitrary expressions is allowed as long as the
-        // expression is known at compile-time.
-        zz => zz,
-        blk: {
-            const d: u32 = 5;
-            const e: u32 = 100;
-            break :blk d + e;
-        } => 107,
-
-        // The else branch catches everything not already captured.
-        // Else branches are mandatory unless the entire range of values
-        // is handled.
-        else => 9,
+test "switch on tagged union" {
+    const Point = struct {
+        x: u8,
+        y: u8,
+    };
+    const Item = union(enum) {
+        a: u32,
+        c: Point,
+        d,
+        e: u32,
     };
 
-    try expect(b == 1);
-}
+    var a = Item{ .c = Point{ .x = 1, .y = 2 } };
 
-// Switch expressions can be used outside a function:
-const os_msg = switch (builtin.target.os.tag) {
-    .linux => "we found a linux user",
-    else => "not a linux user",
-};
+    // Switching on more complex enums is allowed.
+    const b = switch (a) {
+        // A capture group is allowed on a match, and will return the enum
+        // value matched. If the payload types of both cases are the same
+        // they can be put into the same switch prong.
+        Item.a, Item.e => |item| item,
 
-// Inside a function, switch statements implicitly are compile-time
-// evaluated if the target expression is compile-time known.
-test "switch inside function" {
-    switch (builtin.target.os.tag) {
-        .fuchsia => {
-            // On an OS other than fuchsia, block is not even analyzed,
-            // so this compile error is not triggered.
-            // On fuchsia this compile error would be triggered.
-            @compileError("fuchsia not supported");
+        // A reference to the matched value can be obtained using `*` syntax.
+        Item.c => |*item| blk: {
+            item.*.x += 1;
+            break :blk 6;
         },
-        else => {},
-    }
+
+        // No else is required if the types cases was exhaustively handled
+        Item.d => 8,
+    };
+
+    try expect(b == 6);
+    try expect(a.c.x == 2);
 }
