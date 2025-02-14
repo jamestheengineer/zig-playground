2cdd314a6d75f14617dd2770cfbc39c08a01e30f
jamestheengineer <jamestheengineer@gmail.com>
2025-02-14 18:17:37 -0500

    simple while

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,26 +1,9 @@
-const std = @import("std");
+const expect = @import("std").testing.expect;
 
-test "switch continue, equivalent loop" {
-    var sw: i32 = 5;
-    while (true) {
-        switch (sw) {
-            5 => {
-                sw = 4;
-                continue;
-            },
-            2...4 => |v| {
-                if (v > 3) {
-                    sw = 2;
-                    continue;
-                } else if (v == 3) {
-                    break;
-                }
-
-                sw = 1;
-                continue;
-            },
-            1 => return,
-            else => unreachable,
-        }
+test "while basic" {
+    var i: usize = 0;
+    while (i < 10) {
+        i += 1;
     }
+    try expect(i == 10);
 }
