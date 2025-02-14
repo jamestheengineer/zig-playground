7ff210d4298d88a6e760eaaaf45760bba4889685
jamestheengineer <jamestheengineer@gmail.com>
2025-02-13 20:09:55 -0500

    labeled switch

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,18 +1,26 @@
 const std = @import("std");
-const expect = std.testing.expect;
 
-const Color = enum {
-    auto,
-    off,
-    on,
-};
+test "switch continue, equivalent loop" {
+    var sw: i32 = 5;
+    while (true) {
+        switch (sw) {
+            5 => {
+                sw = 4;
+                continue;
+            },
+            2...4 => |v| {
+                if (v > 3) {
+                    sw = 2;
+                    continue;
+                } else if (v == 3) {
+                    break;
+                }
 
-test "enum literals with switch" {
-    const color = Color.off;
-    const result = switch (color) {
-        .auto => false,
-        .on => false,
-        .off => true,
-    };
-    try expect(result);
+                sw = 1;
+                continue;
+            },
+            1 => return,
+            else => unreachable,
+        }
+    }
 }
