20ec9f87c9dabb4023feacd74945041016d30913
jamestheengineer <jamestheengineer@gmail.com>
2025-02-22 09:56:32 -0500

    no return type

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,9 +1,8 @@
-// unreachable is used to assert that control flow will never reach a
-// particular location:
-test "basic math" {
-    const x = 1;
-    const y = 2;
-    if (x + y != 3) {
-        unreachable;
-    }
+fn foo(condition: bool, b: u32) void {
+    const a = if (condition) b else return;
+    _ = a;
+    @panic("do something with a");
+}
+test "noreturn" {
+    foo(false, 1);
 }
