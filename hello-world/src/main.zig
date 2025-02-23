f29faf8b60b5bbd01a093e7bcbf129fbada32663
jamestheengineer <jamestheengineer@gmail.com>
2025-02-23 09:55:35 -0500

    pass by value

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,8 +1,18 @@
-fn foo(condition: bool, b: u32) void {
-    const a = if (condition) b else return;
-    _ = a;
-    @panic("do something with a");
+const Point = struct {
+    x: i32,
+    y: i32,
+};
+
+fn foo(point: Point) i32 {
+    // Here, `point` could be a reference, or a copy. The function body
+    // can ignore the difference and treat it as a value. Be very careful
+    // taking the address of the parameter - it should be treated as if
+    // the address will become invalid when the function returns.
+    return point.x + point.y;
 }
-test "noreturn" {
-    foo(false, 1);
+
+const expect = @import("std").testing.expect;
+
+test "pass struct to function" {
+    try expect(foo(Point{ .x = 1, .y = 2 }) == 3);
 }
