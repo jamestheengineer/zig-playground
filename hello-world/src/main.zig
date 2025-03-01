1f46c66fd97251809ed14da32aecb7bf1e1cf4e3
jamestheengineer <jamestheengineer@gmail.com>
2025-03-01 08:30:09 -0500

    comptime and generics

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,16 +1,9 @@
-const std = @import("std");
-const expect = std.testing.expect;
-
-test "turn HashMap into a set with void" {
-    var map = std.AutoHashMap(i32, void).init(std.testing.allocator);
-    defer map.deinit();
-
-    try map.put(1, {});
-    try map.put(2, {});
-
-    try expect(map.contains(2));
-    try expect(!map.contains(3));
-
-    _ = map.remove(2);
-    try expect(!map.contains(2));
+fn max(comptime T: type, a: T, b: T) T {
+    return if (a > b) a else b;
+}
+fn gimmeTheBiggerFloat(a: f32, b: f32) f32 {
+    return max(f32, a, b);
+}
+fn gimmeTheBiggerInteger(a: u64, b: u64) u64 {
+    return max(u64, a, b);
 }
